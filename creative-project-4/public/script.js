275f004f97491a7494d412d8a1f878e9ccf0cca1
Dallas Williamson <i.am.dallas@hotmail.com>
2018-03-16 11:24:39 -0600

    Updated

--- creative-project-4/public/script.js
+++ creative-project-4/public/script.js
@@ -5,6 +5,11 @@ var app = new Vue({
     text: '',
     show: 'all',
     drag: {},
+    picked: 1,
+    order: '',
+  },
+  created: function(){
+    this.getItems();
   },
   computed: {
     activeItems: function() {
@@ -26,16 +31,75 @@ var app = new Vue({
   },
   methods: {
     addItem: function() {
-      this.items.push({text: this.text,completed:false});
-      this.text = '';
+      axios.post("/api/items", {
+        text: this.text,
+        priority: this.picked,
+        completed: false
+      }).then(response => {
+        this.text = "";
+        this.getItems();
+        return true;
+      }).catch(err => {});
     },
     completeItem: function(item) {
-      item.completed = !item.completed;
+      axios.put("/api/items/" + item.id, {
+        text: item.text,
+        priority: item.priority,
+        completed: !item.completed,
+        orderChange: false,
+      }).then(response => {
+        return true;
+      }).catch(err => {});
     },
     deleteItem: function(item) {
-      var index = this.items.indexOf(item);
-      if (index > -1)
-	this.items.splice(index,1);
+      axios.delete("/api/items/" + item.id).then(response => {
+        this.getItems();
+        return true;
+      }).catch(err => {});
+    },
+    increasePriority: function(item) {
+      if (item.priority < 3) item.priority += 1;
+      axios.put("/api/items/" + item.id, {
+        text: item.text,
+        priority: item.priority,
+        completed: item.completed
+      }).then(response => {
+        this.getItems();
+        return true;
+      }).catch(err => {});
+    },
+    decreasePriority: function(item) {
+      if (item.priority > 1) item.priority -= 1;
+      axios.put("/api/items/" + item.id, {
+        text: item.text,
+        priority: item.priority,
+        completed: item.completed
+      }).then(response => {
+        this.getItems();
+        return true;
+      }).catch(err => {});
+    },
+    sortByPriority: function(){
+      var sortFunc;
+      if (this.order === ''){
+        sortFunc = (a,b) => {
+          if (a.priority > b.priority) return 1;
+          else if (a.priority < b.priority) return -1
+          else return a.text < b.text ? -1 : a.text > b.text
+        };
+        this.order = '^';
+      } else if (this.order === '^') {
+        sortFunc = (a,b) => {
+          if (a.priority < b.priority) return 1;
+          else if (a.priority > b.priority) return -1
+          else return a.text < b.text ? -1 : a.text > b.text
+        };
+        this.order = 'v';
+      } else {
+        this.getItems();
+        this.order = '';
+      }
+      this.items.sort(sortFunc);
     },
     showAll: function() {
       this.show = 'all';
@@ -47,18 +111,30 @@ var app = new Vue({
       this.show = 'completed';
     },
     deleteCompleted: function() {
-      this.items = this.items.filter(function(item) {
-	return !item.completed;
+      this.items.forEach(item => {
+	if (item.completed)
+	  this.deleteItem(item)
       });
     },
     dragItem: function(item) {
       this.drag = item;
     },
     dropItem: function(item) {
-      var indexItem = this.items.indexOf(this.drag);
-      var indexTarget = this.items.indexOf(item);
-      this.items.splice(indexItem,1);
-      this.items.splice(indexTarget,0,this.drag);
+      axios.put("/api/items/" + this.drag.id, {
+        text: this.drag.text,
+        completed: this.drag.completed,
+        orderChange: true,
+        orderTarget: item.id
+      }).then(response => {
+        this.getItems();
+        return true;
+      }).catch(err => {});
+    },
+    getItems: function(){
+      axios.get("/api/items").then(response => {
+        this.items = response.data;
+        return true;
+      }).catch(err => {});
     },
   }
 });
--- creative-project-4/server.js
+++ creative-project-4/server.js
@@ -16,9 +16,37 @@ app.get('/api/items', (req, res) => {
 
 app.post('/api/items', (req, res) => {
   id = id + 1;
-  let item = {id:id, text:req.body.text, completed: req.body.completed};
+  let item = {id:id, text:req.body.text, priority:req.body.priority, completed: req.body.completed};
   items.push(item);
   res.send(item);
 });
 
-app.listen(3001, () => console.log('Server listening on port 3001!'))
+app.put('/api/items/:id', (req, res) => {
+  let id = parseInt(req.params.id);
+  let itemsMap = items.map(item => { return item.id; });
+  let index = itemsMap.indexOf(id);
+  let item = items[index];
+  item.completed = req.body.completed;
+  item.text = req.body.text;
+  item.priority = req.body.priority;
+  // handle drag and drop re-ordering
+  if (req.body.orderChange) {
+    let indexTarget = itemsMap.indexOf(req.body.orderTarget);
+    items.splice(index,1);
+    items.splice(indexTarget,0,item);
+  }
+  res.send(item);
+});
+
+app.delete('/api/items/:id', (req, res) => {
+  let id = parseInt(req.params.id);
+  let removeIndex = items.map(item => { return item.id; }).indexOf(id);
+  if (removeIndex === -1) {
+    res.status(404).send("Sorry, that item doesn't exist");
+    return;
+  }
+  items.splice(removeIndex, 1);
+  res.sendStatus(200);
+});
+
+app.listen(3001, () => console.log('Server listening on port 3000!'))
